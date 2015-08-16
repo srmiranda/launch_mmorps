31df4357c3ceee12ea516dec1ceb727b820a0981
SasquatchShawn <srmiranda1@gmail.com>
2015-08-15 22:20:49 -0400

    Refactored slightly. Added flash messages.

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require 'pry'
+require "sinatra/flash"
 
 use Rack::Session::Cookie, {
   secret: "keep_it_secret_keep_it_safe"
@@ -19,12 +20,12 @@ def score
   until session[:player_score] == 2 || session[:computer_score] == 2
     if (session[:player_choice] == "Rock" && session[:computer_pick] == "Scissors") || (session[:player_choice] == "Paper" && session[:computer_pick] == "Rock") || (session[:player_choice] == "Scissors" && session[:computer_pick] == "Paper")
       session[:player_score] += 1
-      session[:round_message] = "Player won."
+      flash[:message] = "Player won."
     elsif (session[:computer_pick] == "Rock" && session[:player_choice] == "Scissors") || (session[:computer_pick] == "Paper" && session[:player_choice] == "Rock") || (session[:computer_pick] == "Scissors" && session[:player_choice] == "Paper")
       session[:computer_score] += 1
-      session[:round_message] = "Computer won."
+      flash[:message] = "Computer won."
     elsif (session[:computer_pick] == session[:player_choice])
-      session[:round_message] = "Tie, choose again."
+      flash[:message] = "Tie, choose again."
     end
   redirect "/"
  end
@@ -32,9 +33,9 @@ end
 
 def game_winner
   if session[:player_score] >= 2
-    params[:winner_message] = "Player wins the game!"
+    "Player wins the game!"
   else
-    params[:winner_message] = "Computer wins the game. Better luck next time!"
+    "Computer wins the game. Better luck next time!"
   end
 end
 
@@ -44,8 +45,7 @@ get '/' do
   end
   game_winner
 
-  erb :index, locals: { winner_message: session[:winner_message],
-    round_message: session[:round_message], player_choice: session[:player_choice],
+  erb :index, locals: { message: flash[:message], player_choice: session[:player_choice],
     player_score: session[:player_score], computer_score: session[:computer_score] }
 end
 
